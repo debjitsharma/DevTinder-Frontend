c21c5d571b1bf7f9a4177c368911f3bd959d2bf5
debjitsharma <debjit2k15@gmail.com>
2026-04-05 12:53:47 +0530

    useeffects

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import { BASE_URL } from '../utils/constants'
 
 export default function Body() {
+  const dispatch= useDispatch();
+  const navigate= Navigate();
+  const fetchUser= async()=>{
+    try{
+    const res= await axios.get(BASE_URL+ "/profile",{
+      withCredentials:true});
+  
+  dispatch(addUser(res.data))}
+  catch(err){
+    if (err.status===401){
+      navigate("/login")
+    }
+  }};
+  useEffect(()=>{
+    fetchUser();
+  },[]);
   return (
     <div><Navbar/>
     <Outlet/>
