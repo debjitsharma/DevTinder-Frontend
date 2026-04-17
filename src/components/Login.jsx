d9bc8e08819c22b452f49e74096a538b7c2e5365
debjitsharma <debjit2k15@gmail.com>
2026-04-17 08:45:29 +0530

    login signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -61,10 +61,16 @@ const Login = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+               {isLoginForm?"Sign in to your account":"Log in to your account"}
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                 <div>
+                {!isLoginForm && (<>       <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    First Name
+                  </label>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Last Name
+                  </label></>)}
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Your email
                   </label>
